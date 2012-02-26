9f7bbc225b526feadff7ccbea5bcbc7d68b74a9b
Jozef Zigmund <hypnopd@gmail.com>
2012-02-26 02:06:20 +0100

    Edited USER migration according to the newest Devise migration scheme style

--- db/migrate/20111201221537_devise_create_users.rb
+++ db/migrate/20111201221537_devise_create_users.rb
@@ -1,16 +1,36 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+      ## Database authenticatable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Lockable
+      t.integer  :failed_attempts, :default => 0, :unlock_strategy => :both, :maximum_attempts => 5, :unlock_in => 10 # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
+
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
 
       # t.encryptable
-      t.confirmable
-      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
-
 
       t.timestamps
     end
