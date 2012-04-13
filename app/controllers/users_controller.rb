f370bf4422d15bc90612fa4d729778f7bee87459
Jozef Zigmund <jzigmund@redhat.com>
2012-04-13 02:24:49 +0200

    UsersController: Removed unwanted actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,6 @@ class UsersController < ApplicationController
     @users = Users.all
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 end
