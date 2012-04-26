99f540981c3de45497ecb501d643ebf1e288ab2e
Jozef Zigmund <hypnopd@gmail.com>
2012-04-26 19:15:52 +0200

    Seeds: Added companies and attr accessible company to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :confirmed_at, :role
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :confirmed_at, :role, :company
   has_ancestry
   belongs_to :company
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,65 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+#COMPANIES
+#default company of creators and admins
+puts "Creation of Foobar company"
+default_company = Company.create! :name => "FooBar"
+puts "Company Foobar created"
+
+puts "Creation of WannaWin company"
+wannawin = Company.create! :name => "WannaWin"
+puts "Company WannaWin created"
+
+#USERS
 #default admin
 puts "Creation of Admin user"
-admin = User.create! :email => "admin@admin.com", :password => "admin11", :password_confirmation => "admin11",
-                     :confirmed_at => DateTime.now, :role => "admin"
-admin.update_attribute :parent, admin
-puts "Created Admin user: " << admin.email
+admin = User.create! :email => "admin@admin.com",
+                     :password => "admin11",
+                     :password_confirmation => "admin11",
+                     :confirmed_at => DateTime.now,
+                     :role => User::ROLES[:admin],
+                     :company => default_company
+#default creator user
+puts "Creation of Creator user"
+creator = User.create! :email => "creator@creator.com",
+                       :password => "creator11",
+                       :password_confirmation => "creator11",
+                       :confirmed_at => DateTime.now,
+                       :role => User::ROLES[:creator],
+                       :company => default_company
+creator.update_attribute :parent, admin
+puts "Created Creator user: " << creator.email
 
-#default user
-puts "Creation of User"
-user = User.create! :email => "user@user.com", :password => "user11", :password_confirmation => "user11",
-                    :confirmed_at => DateTime.now, :role => "creator"
-user.update_attribute :parent, admin
-puts "Created User: " << user.email
+#default competitor user
+puts "Creation of Competitor user"
+competitor = User.create! :email => "comp@comp.com",
+                          :password => "comp11",
+                          :password_confirmation => "comp11",
+                          :confirmed_at => DateTime.now,
+                          :role => User::ROLES[:competitor],
+                          :company => wannawin
+competitor.update_attribute :parent, admin
+puts "Created Competitor user: " << competitor.email
+
+#default creator_viewer user
+puts "Creation of Creator_viewer user"
+creator_viewer = User.create! :email => "viewercr@viewercr.com",
+                              :password => "viewercr11",
+                              :password_confirmation => "viewercr11",
+                              :confirmed_at => DateTime.now,
+                              :role => User::ROLES[:creator_viewer],
+                              :company => default_company
+creator_viewer.update_attribute :parent, admin
+puts "Created Creator_viewer user: " << creator_viewer.email
+
+#default competitor_viewer user
+puts "Creation of Competitor_viewer user"
+competitor_viewer = User.create! :email => "viewercomp@viewercomp.com",
+                                 :password => "viewercomp11",
+                                 :password_confirmation => "viewercomp11",
+                                 :confirmed_at => DateTime.now,
+                                 :role => User::ROLES[:competitor_viewer],
+                                                      :company => wannawin
+competitor_viewer.update_attribute :parent, admin
+puts "Created Competitor_viewer user: " << competitor_viewer.email
