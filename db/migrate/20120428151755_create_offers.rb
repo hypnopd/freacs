ddd11dfe16003d2d2b117c71432472b5ef06e657
Jozef Zigmund <hypnopd@gmail.com>
2012-04-30 00:42:32 +0200

    Offers migration: added polymorphic and auction_user_id

--- db/migrate/20120428151755_create_offers.rb
+++ db/migrate/20120428151755_create_offers.rb
@@ -1,7 +1,8 @@
 class CreateOffers < ActiveRecord::Migration
   def change
     create_table :offers do |t|
-
+      t.references :offerable, :polymorphic => true
+      t.references :auction_user
       t.timestamps
     end
   end
--- db/migrate/20120429180431_add_offerable_to_items_conditions.rb
+++
@@ -1,7 +0,0 @@
-class AddOfferableToItemsConditions < ActiveRecord::Migration
-  def change
-    change_table :offers do |t|
-      t.references :offerable, :polymorphic => true
-    end
-  end
-end
