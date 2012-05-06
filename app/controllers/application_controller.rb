57f79bff80c1c1f3a9901a20f4663abfa13000ce
Jozef Zigmund <hypnopd@gmail.com>
2012-05-07 00:25:48 +0200

    AppController: Rescue from 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-      redirect_to root_url, :alert => t("cancan.not_authorized")
+    redirect_to root_url, :alert => t("cancan.not_authorized")
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    redirect_to root_url, :alert => t("exceptions.not_found")
   end
 
 
